8b150125bf6d385324357740d6484b8e3fd9cad6
Aiden <dwx@AidendeMacBook-Pro.local>
2024-06-13 22:13:56 +0800

    i

--- internal/vite-config/src/plugins/index.ts
+++ internal/vite-config/src/plugins/index.ts
@@ -3,7 +3,8 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 import { type PluginOption } from 'vite';
 import purgeIcons from 'vite-plugin-purge-icons';
 import DevTools from 'vite-plugin-vue-devtools';
-
+import path from 'node:path';
+import electron from 'vite-plugin-electron/simple';
 import { createAppConfigPlugin } from './appConfig';
 import { configCompressPlugin } from './compress';
 import { configHtmlPlugin } from './html';
@@ -55,7 +56,27 @@ async function createPlugins({ isBuild, root, enableMock, compress, enableAnalyz
   if (enableMock) {
     vitePlugins.push(configMockPlugin({ isBuild }));
   }
-
+  vitePlugins.push(
+    electron({
+      main: {
+        // Shortcut of `build.lib.entry`.
+        entry: path.resolve(__dirname, '../../../../electron/main.ts'),
+      },
+      preload: {
+        // Shortcut of `build.rollupOptions.input`.
+        // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
+        input: path.resolve(__dirname, '../../../../electron/preload.ts'),
+      },
+      // Ployfill the Electron and Node.js API for Renderer process.
+      // If you want use Node.js in Renderer process, the `nodeIntegration` needs to be enabled in the Main process.
+      // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
+      renderer:
+        process.env.NODE_ENV === 'test'
+          ? // https://github.com/electron-vite/vite-plugin-electron-renderer/issues/78#issuecomment-2053600808
+            undefined
+          : {},
+    }),
+  );
   return vitePlugins;
 }
 
