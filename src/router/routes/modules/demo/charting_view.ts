f8af405277364688201f2988827e2b8e0e917765
Aiden Dong <dwx_job@163.com>
2024-06-21 19:54:13 +0800

    i

---
+++ src/router/routes/modules/demo/charting_view.ts
@@ -0,0 +1,89 @@
+import type { AppRouteModule } from '@/router/types';
+import { LAYOUT } from '@/router/constant';
+
+const IFrame = () => import('@/views/sys/iframe/FrameBlank.vue');
+
+const charts: AppRouteModule = {
+  path: '/charting_view',
+  name: 'charting_view',
+  component: LAYOUT,
+  redirect: '/charting_view/echarts/map',
+  meta: {
+    orderNo: 1,
+    title: 'charting',
+    icon: 'ant-design:stock-outlined',
+  },
+  children: [
+    {
+      path: 'charting_library',
+      name: 'charting_library',
+      meta: {
+        title: 'ChartingLibrary',
+        icon: 'ant-design:stock-outlined',
+      },
+      component: () => import('@/views/charting/ChartingLibrary.vue'),
+    },
+    {
+      path: 'tonghuashun',
+      name: 'tonghuashun',
+      component: IFrame,
+      meta: {
+        frameSrc: 'https://q.10jqka.com.cn/',
+        title: '行情中心',
+        icon: 'ant-design:stock-outlined',
+      },
+    },
+    {
+      path: 'tonghuashun_gn',
+      name: 'tonghuashun_gn',
+      component: IFrame,
+      meta: {
+        frameSrc: 'http://q.10jqka.com.cn/gn/',
+        title: '概念',
+        icon: 'ant-design:stock-outlined',
+      },
+    },
+    {
+      path: 'tonghuashun_bk',
+      name: 'tonghuashun_bk',
+      component: IFrame,
+      meta: {
+        frameSrc: 'http://q.10jqka.com.cn/thshy/',
+        title: '行业',
+        icon: 'ant-design:stock-outlined',
+      },
+    },
+    {
+      path: 'tonghuashun_longhu',
+      name: 'tonghuashun_longhu',
+      component: IFrame,
+      meta: {
+        frameSrc: 'http://data.10jqka.com.cn/market/longhu/',
+        title: '龙虎榜',
+        icon: 'ant-design:stock-outlined',
+      },
+    },
+    {
+      path: 'tonghuashun_hongkong',
+      name: 'tonghuashun_hongkong',
+      component: IFrame,
+      meta: {
+        frameSrc: 'http://q.10jqka.com.cn/hk/',
+        title: '香港',
+        icon: 'ant-design:stock-outlined',
+      },
+    },
+    {
+      path: 'tonghuashun_jishu',
+      name: 'tonghuashun_jishu',
+      component: IFrame,
+      meta: {
+        frameSrc: 'http://data.10jqka.com.cn/rank/cxg/',
+        title: '技术',
+        icon: 'ant-design:stock-outlined',
+      },
+    },
+  ],
+};
+
+export default charts;
---
+++ src/views/charting/ChartingLibrary.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="TVChartContainer" ref="chartContainer"></div>
+</template>
+<script setup>
+    import { onMounted, ref, onUnmounted } from 'vue';
+    import Datafeed from '@/components/trading_view/datafeed/datafeed';
+    import { isFirst } from '@/api/tradingview/tradingview';
+    function getLanguageFromURL() {
+      const regex = new RegExp('[\\?&]lang=([^&#]*)');
+      const results = regex.exec(window.location.search);
+      return results === null ? null : decodeURIComponent(results[1].replace(/\+/g, ' '));
+    }
+
+    const props = defineProps({
+      symbol: {
+        default: 'AAPL',
+        type: String,
+      },
+      interval: {
+        default: 'D',
+        type: String,
+      },
+      libraryPath: {
+        default: '/charting_library/',
+        type: String,
+      },
+      chartsStorageUrl: {
+        // default: 'http://localhost:10008/tradingview',
+        default: '/basic-api/tradingview',
+        type: String,
+      },
+      chartsStorageApiVersion: {
+        default: '1.0',
+        type: String,
+      },
+      clientId: {
+        default: 'tradingview.com',
+        type: String,
+      },
+      userId: {
+        default: '1',
+        type: String,
+      },
+      fullscreen: {
+        default: false,
+        type: Boolean,
+      },
+      autosize: {
+        default: true,
+        type: Boolean,
+      },
+      studiesOverrides: {
+        type: Object,
+      },
+    });
+
+    const chartContainer = ref();
+    let chartWidget;
+
+    onMounted(async () => {
+      const isFirstIn = await isFirst({ clientId: props.clientId });
+
+      const widgetOptions = {
+        timezone: 'Asia/Shanghai',
+        load_last_chart: true,
+        datafeed: Datafeed,
+        interval: props.interval,
+        container: chartContainer.value,
+        library_path: props.libraryPath,
+
+        locale: getLanguageFromURL() || 'zh',
+        disabled_features: ['use_localstorage_for_settings'],
+        enabled_features: ['study_templates'],
+        charts_storage_url: props.chartsStorageUrl,
+        charts_storage_api_version: props.chartsStorageApiVersion,
+        client_id: props.clientId,
+        user_id: props.userId,
+        fullscreen: props.fullscreen,
+        autosize: props.autosize,
+        studies_overrides: props.studiesOverrides,
+        // eslint-disable-next-line no-dupe-keys
+        disabled_features: [
+          'use_localstorage_for_settings',
+          'open_account_manager',
+          'trading_account_manager',
+        ],
+        // eslint-disable-next-line no-dupe-keys
+        enabled_features: [
+          'hide_right_toolbar',
+          'pre_post_market_sessions',
+          'show_symbol_logos',
+          'show_exchange_logos',
+          'seconds_resolution',
+          'tick_resolution',
+          'chart_template_storage',
+          'secondary_series_extend_time_scale',
+          // 'show_percent_option_for_right_margin',
+          // 'display_data_mode',
+          // 'items_favoriting',
+          // 'hide_left_toolbar_by_default',
+          'study_templates',
+          // 'pre_post_market_sessions',
+          'show_object_tree',
+        ],
+      };
+      if (isFirstIn) {
+        widgetOptions.symbol = 'SH:600000/RMB';
+      }
+      chartWidget = new TradingView.widget(widgetOptions);
+
+      chartWidget.onChartReady(() => {
+        chartWidget.headerReady().then(() => {
+          const button = chartWidget.createButton();
+
+          button.setAttribute('title', 'Click to show a notification popup');
+          button.classList.add('apply-common-tooltip');
+
+          button.addEventListener('click', () =>
+            chartWidget.showNoticeDialog({
+              title: 'Notification',
+              body: 'TradingView Charting Library API works correctly',
+              callback: () => {
+                // eslint-disable-next-line no-console
+                console.log('Noticed!');
+              },
+            }),
+          );
+
+          button.innerHTML = 'Check API';
+
+          var button1 = chartWidget.createButton();
+          button1.setAttribute('title', 'My custom button tooltip');
+          button1.addEventListener('click', function () {
+            chartWidget.activeChart().executeActionById('drawingToolbarAction');
+          });
+          button1.textContent = '绘图';
+        });
+      });
+    });
+
+    onUnmounted(() => {
+      if (chartWidget !== null) {
+        chartWidget.remove();
+        chartWidget = null;
+      }
+    });
+</script>
+
+<style scoped>
+  .TVChartContainer {
+    height: calc(100vh - 80px);
+  }
+</style>
