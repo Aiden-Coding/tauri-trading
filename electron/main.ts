b621ac965e03c54cdb4a8bb3ffc313add110aef0
Aiden Dong <dwx_job@163.com>
2024-06-29 17:32:17 +0800

    update-trading-view

--- electron/main.ts
+++ electron/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, ipcMain, screen } from 'electron';
+import { app, BrowserWindow, ipcMain, MessageChannelMain } from 'electron';
 // import { createRequire } from 'node:module'
 import { fileURLToPath } from 'node:url';
 import path from 'node:path';
@@ -36,16 +36,16 @@ let win: BrowserWindow | null;
 let workerProcess: ChildProcess | null;
 function createWindow() {
   // Renderer 3
-  const nodeFalse = new BrowserWindow({
+  const worker = new BrowserWindow({
     show: false,
     webPreferences: { nodeIntegration: true },
   });
   if (process.env.VITE_DEV_SERVER_URL) {
-    nodeFalse.loadURL('http://localhost:5175/html/node-false.html');
+    worker.loadURL('http://localhost:5175/html/node-false.html');
   } else {
-    nodeFalse.loadFile(path.join(__dirname, '../dist/html/node-false.html'));
+    worker.loadFile(path.join(__dirname, '../dist/html/node-false.html'));
   }
-  windows.push(nodeFalse);
+  windows.push(worker);
   win = new BrowserWindow({
     width: 1000,
     // resizable: true,
@@ -77,57 +77,7 @@ function createWindow() {
     // win.loadFile('dist/index.html')
     win.loadFile(path.join(RENDERER_DIST, 'index.html'));
   }
-  /** 窗口移动功能封装 */
-  // 窗口移动 位置刷新定时器
-  let movingInterval: NodeJS.Timeout | null;
-  /**
-   * 窗口移动事件
-   */
-  ipcMain.on('window-move-open', (event, canMoving) => {
-    // 获取当前鼠标聚焦的窗口
-    const currentWindow = BrowserWindow.getFocusedWindow();
 
-    if (currentWindow) {
-      const currentWindowSize = currentWindow.getSize();
-      if (canMoving) {
-        // 读取原位置
-        const winPosition = currentWindow.getPosition();
-        let winStartPosition = screen.getCursorScreenPoint();
-        // 清除旧的定时器
-        if (movingInterval) {
-          clearInterval(movingInterval);
-        }
-        // 创建定时器，每10毫秒更新一次窗口位置，保证一致
-        movingInterval = setInterval(() => {
-          // 窗口销毁判断，高频率的更新有可能窗口已销毁，定时器还没结束，此时就会出现执行销毁窗口方法的错误
-          if (!currentWindow.isDestroyed()) {
-            // 如果窗口失去焦点，则停止移动
-            if (!currentWindow.isFocused()) {
-              if (movingInterval) clearInterval(movingInterval);
-              movingInterval = null;
-            }
-            // 实时更新位置
-            const cursorPosition = screen.getCursorScreenPoint();
-            const x = cursorPosition.x - winStartPosition.x + winPosition[0];
-            const y = cursorPosition.y - winStartPosition.y + winPosition[1];
-            // 更新位置的同时设置窗口原大小， windows上设置=>显示设置=>文本缩放 不是100%时，窗口会拖拽放大
-            currentWindow.setBounds({
-              x: x,
-              y: y,
-              width: currentWindowSize[0],
-              height: currentWindowSize[1],
-            });
-          } else {
-            if (movingInterval) clearInterval(movingInterval);
-            movingInterval = null;
-          }
-        }, 10);
-      } else {
-        if (movingInterval) clearInterval(movingInterval);
-        movingInterval = null;
-      }
-    }
-  });
   workerProcess = child_process.exec(
     'start /b C:\\Users\\Administrator\\.jdks\\openjdk-21.0.1\\bin\\java.exe -jar trading-life-java.jar',
     function (error, stdout, stderr) {
@@ -151,6 +101,18 @@ function createWindow() {
     console.log('child_process exit ');
   });
   console.log(workerProcess.pid);
+  // 在这里我们不能使用 ipcMain.handle() , 因为回复需要传输
+  // MessagePort.
+  // 监听从顶级 frame 发来的消息
+  win.webContents.mainFrame.ipc.on('request-worker-channel', (event) => {
+    // 建立新通道  ...
+    const { port1, port2 } = new MessageChannelMain();
+    // ... 将其中一个端口发送给 Worker ...
+    worker.webContents.postMessage('new-client', null, [port1]);
+    // ... 将另一个端口发送给主窗口
+    event.senderFrame.postMessage('provide-worker-channel', null, [port2]);
+    // 现在主窗口和工作进程可以直接相互通信，无需经过主进程！
+  });
 }
 
 // Quit when all windows are closed, except on macOS. There, it's common
